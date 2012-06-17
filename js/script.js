2a468fe57abda7dd09c408fa1c427f19a3a80659
David Ziegler <dzieglersf@gmail.com>
2012-06-17 16:25:49 -0700

    added gravity like effect to death plunge

--- js/script.js
+++ js/script.js
@@ -1,55 +1,52 @@
-/* Author:
-
-Gus Cost & David Ziegler
-
+/* A bird that flies and dies when you click him
+By Gus Cost & David Ziegler
 */
 
-var paper = Raphael(0, 0, $(window).width(), $(window).height());
+var window_width = $(window).width();
+var window_height = $(window).height();
+
+var paper = Raphael(0, 0, window_width, window_height);
 
 //SCENERY
-var sky = paper.rect(0,0,$(window).width(), $(window).height());
+var sky = paper.rect(0,0,window_width, window_height);
 sky.attr("fill", "90-#66D:5-#88F:95");
 
-var hill = paper.circle($(window).width(), $(window).height() + 1400, 1600);
+var hill = paper.circle(window_width, window_height + 1400, 1600);
 hill.attr("fill", "#0A0");
 hill.attr("stroke", "#fff");
 
-var hill2 = paper.circle(100, $(window).height() + 1420, 1700);
+var hill2 = paper.circle(100, window_height + 1420, 1700);
 hill2.attr("fill", "#091");
 hill2.attr("stroke", "#fff");
 
-var trunk = paper.rect($(window).width() - 140, $(window).height() - 420, 50, 250);
+var trunk = paper.rect(window_width - 140, window_height - 420, 50, 250);
 trunk.attr("fill", "#440");
 trunk.attr("stroke", "#330");
 
 for (var x = 0; x < 300; x++)
 {
-        var t = 2*Math.PI*Math.random();
-        var u = Math.random()+Math.random();
-        var r = u>1 ? 2-u : u;
-        var theX = r*Math.cos(t);
-        var theY =  r*Math.sin(t);
-        // Creates hill at x = 50, y = 40, with radius 10
-        var leaf = paper.circle(($(window).width() - 115 + theX*120), ($(window).height() - 420 + theY*120), 15);
-        // Sets the fill attribute of the hill to red (#f00)
-        leaf.attr("fill", "#0e0");
-        // Sets the stroke attribute of the hill to white
-        leaf.attr("stroke", "#0a0");
+  var t = 2*Math.PI*Math.random();
+  var u = Math.random()+Math.random();
+  var r = u>1 ? 2-u : u;
+  var theX = r*Math.cos(t);
+  var theY =  r*Math.sin(t);
+  // Creates leaf at x = 50, y = 40, with radius 10
+  var leaf = paper.circle((window_width - 115 + theX*120), (window_height - 420 + theY*120), 15);
+  // Sets the fill attribute of the leaf to green 
+  leaf.attr("fill", "#060");
+  // Sets the stroke attribute of the leaf to light green
+  leaf.attr("stroke", "#0c0");
 }
 
 // THE BIRD
-
-//a circle for now
-var width = $(window).width();
-var height = $(window).height();
 
 var number_of_frames = 100;
 
 var bird_start_X = 10;
 var bird_start_Y = 10;
 
-var bird_end_X = width - 115; // from gus's trunk above, middle of it
-var bird_end_Y = height - 420; // from gus's trunk above, top of it
+var bird_end_X = window_width - 115; // from gus's trunk above, middle of it
+var bird_end_Y = window_height - 420; // from gus's trunk above, top of it
 
 var bird_x = bird_start_X;
 var bird_y = bird_start_Y;
@@ -68,38 +65,44 @@ bird[1].click(function(){bird_is_alive = false;});
 bird[2].click(function(){bird_is_alive = false;});
 bird[3].click(function(){bird_is_alive = false;});
 
-var translate_interval_X = (bird_end_X - bird_start_X) / number_of_frames;
-var translate_interval_Y = (bird_end_Y - bird_start_Y) / number_of_frames;
+var dx = (bird_end_X - bird_start_X) / number_of_frames;
+var dy = (bird_end_Y - bird_start_Y) / number_of_frames;
 
 var frame_number = 0
 var sprite_number = 0;
 
+var dt = 100;
+var death_ticks = 1;
+var death_y = window_height - 100;
 
 window.setInterval(function() {
 	
 	if (bird_is_alive)
 	{
-	if (frame_number < number_of_frames) {
-		bird_x = bird_x + translate_interval_X;
-		bird_y = bird_y + translate_interval_Y;
+		if (frame_number < number_of_frames) {
+			bird_x = bird_x + dx;
+			bird_y = bird_y + dy;
 
-		frame_number++;
+			frame_number++;
 
-		bird[sprite_number].attr({x: 9999, y: 9999});
-		sprite_number = (sprite_number +1) %4;
-		bird[sprite_number].attr({x: bird_x, y: bird_y});
+			bird[sprite_number].attr({x: 9999, y: 9999});
+			sprite_number = (sprite_number +1) % 4;
+			bird[sprite_number].attr({x: bird_x, y: bird_y});
 
+		}
 	}
-	}
-	else if (bird_y < height - 100)
+	else if (bird_y < death_y)
 	{
-		bird_y = bird_y + 20;
+		// bird with gravity pulling him down:
 		bird[sprite_number].attr({x: bird_x, y: bird_y});
+		bird_x = bird_x + dx;
+		bird_y = bird_y + 20 * (death_ticks);
+		death_ticks += 1; 
 	}
 	else
 	{
-		deadbird.attr({x: bird_x, y: bird_y});
+		deadbird.attr({x: bird_x, y: death_y});
 		bird[sprite_number].attr({x: 9999, y: 9999});
 	}
 
-	}, 100);
+	}, dt);
