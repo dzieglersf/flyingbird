a2d9c6f03a320fee190967346f8a33c97aa34730
David Ziegler <dzieglersf@gmail.com>
2012-06-19 00:19:07 -0700

    renamed images, added left-facing, added proper index

--- js/flytotree.js
+++ js/flytotree.js
@@ -45,7 +45,7 @@ var Point = function(x, y) {
 	this.y = y;
 }
 
-// THE BIRD
+// A BIRD
 
 var Bird = function( name, start_point, end_point, flight_time )
 {
@@ -63,12 +63,10 @@ var Bird = function( name, start_point, end_point, flight_time )
 	this.velocity_x = Math.abs( Math.cos(angle)*speed );
 	this.velocity_y = Math.abs( Math.sin(angle)*speed );
 
-	this.freefall_time = 0;
-
 	// bird images and kill event on each image
 	this.flight_image = [];
 	for (i=0; i<4; i++) {
-		this.flight_image[i] = paper.image('img/bird' + i + '.png', 9999, 9999, 40, 40);
+		this.flight_image[i] = paper.image('img/right_bird' + i + '.png', 9999, 9999, 40, 40);
 		this.flight_image[i].click( function() {
 			this.alive = false;
 		});
@@ -85,6 +83,9 @@ var Bird = function( name, start_point, end_point, flight_time )
 				this.x = this.x + (this.velocity_x * dt);
 				this.y = this.y + (this.velocity_y * dt);
 
+// TODO: REMOVE THIS
+// this is sitting in here to test out the falling birds
+// until I figure out what's wrong with the click/kill event
 if (this.x > (start_point.x+end_point.x)/2) {this.alive = false;}
 
 				this.flight_image[this.current_image_index].attr( { x: 9999, y: 9999 } );
@@ -115,16 +116,12 @@ var max_duration_of_animation = 10000; // in milliseconds
 var dt = 50; // refresh rate, in milliseconds
 var time = 0;
 
+// make a set of birds
 var birds = [];
-
-//tweetie
 birds[0] = new Bird("tweetie", new Point(10,10), new Point(window_width - 115, window_height - 420), 10000);
 birds[1] = new Bird("toucansam", new Point(30,30), new Point(window_width - 175, window_height - 100), 8000);
 birds[2] = new Bird("polly", new Point(50, 30), new Point(window_width - 225, window_height - 100), 9000);
 birds[3] = new Bird("woodstock", new Point(10, 50), new Point(window_width - 150, window_height - 420), 5000);
-
-
-var current_bird = null;
 
 var animate = function() {
 	if ( time < max_duration_of_animation ) {
@@ -139,53 +136,5 @@ var animate = function() {
 	}
 }
 
-
+// get it all going
 window.setInterval(animate, dt);
-
-// var number_of_frames = 100;
-
-// var frame_number = 0
-// var sprite_number = 0;
-
-// var dt = 100;
-// var death_ticks = 1;
-// var death_y = window_height - 100;
-
-// var bird_end_X = window_width - 115; // from gus's trunk above, middle of it
-// var bird_end_Y = window_height - 420; // from gus's trunk above, top of it
-
-
-
-// window.setInterval(function() {
-	
-// 	var b = new Bird('tweetie', 10, 10);
-
-// 	if (bird_is_alive)
-// 	{
-// 		if (frame_number < number_of_frames) {
-// 			bird_x = bird_x + dx;
-// 			bird_y = bird_y + dy;
-
-// 			frame_number++;
-
-// 			bird[sprite_number].attr({x: 9999, y: 9999});
-// 			sprite_number = (sprite_number +1) % 4;
-// 			bird[sprite_number].attr({x: bird_x, y: bird_y});
-
-// 		}
-// 	}
-// 	else if (bird_y < death_y)
-// 	{
-// 		// bird with gravity pulling him down:
-// 		bird[sprite_number].attr({x: bird_x, y: bird_y});
-// 		bird_x = bird_x + dx;
-// 		bird_y = bird_y + 20 * (death_ticks);
-// 		death_ticks += 1; 
-// 	}
-// 	else
-// 	{
-// 		deadbird.attr({x: bird_x, y: death_y});
-// 		bird[sprite_number].attr({x: 9999, y: 9999});
-// 	}
-
-// 	}, dt);
